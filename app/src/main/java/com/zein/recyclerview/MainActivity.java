cc850575183bf97bc21b14b035561963165431d0
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-02-26 13:43:27 +0700

    wertyucvbnmasasfasfa

--- app/src/main/java/com/zein/recyclerview/MainActivity.java
+++ app/src/main/java/com/zein/recyclerview/MainActivity.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ////wertyuiopoiuytrtyuio
+
     private static final String TAG = "MainActivity";
 
     private ArrayList<String> mNames = new ArrayList<>();
